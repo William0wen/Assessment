8d62f49f89da2c308e4d668f8073496be323b0d0
William Owen <98859927+William0wen@users.noreply.github.com>
2022-05-05 12:26:21 +1200

    loop update

---
+++ 03_loop_v2.py
@@ -0,0 +1,53 @@
+"""Based off 03_loop_v1, but deletes a
+question from the list after it has already been asked.
+This way, every question will be asked once, and not repeated."""
+
+import random
+
+# List of questions and answers, stored in dictionaries
+questions = [{"day": "Monday", "answer": "rahina"},
+             {"day": "Tuesday", "answer": "ratu"},
+             {"day": "Wednesday", "answer": "raapa"},
+             {"day": "Thursday", "answer": "rapare"},
+             {"day": "Friday", "answer": "ramere"},
+             {"day": "Saturday", "answer": "rahoroi"},
+             {"day": "Sunday", "answer": "ratapu"}]
+
+# Generating random question out of the list of questions
+
+score = 0
+
+
+# Ask question function
+def ask(question_text, answer):
+    global score
+    # Ask question
+    question = input(f"What is the Maori word for {question_text}? ").lower().strip()
+
+    # If answer is correct, continue
+    if question == answer:
+        print("\nCorrect!")
+        score += 1
+        print(f"score: {score}\n")
+
+    # Else, score stays the same, continue
+    else:
+        print("\nWrong answer!")
+        print(f"score: {score}\n")
+
+
+# Main
+
+# Repeating the round 7 times
+for number in questions:
+    # Generating random question out of the list of questions
+    random_question = random.choice(questions)
+    rand_day = random_question["day"]
+    rand_answer = random_question["answer"]
+
+    # question loop
+    ask(rand_day, rand_answer)
+
+    # removing the question that has already been asked.
+    questions.remove(random_question)
+    print(questions)
