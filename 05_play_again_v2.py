5046fb40f8002de353141dbb5f84c6712e446046
William Owen <98859927+William0wen@users.noreply.github.com>
2022-05-10 09:23:13 +1200

    play again v2

--- 05_play_again_v2.py
+++ 05_play_again_v2.py
@@ -1,12 +1,15 @@
 """Simplifies input with .lower()
 and .strip()"""
 
+repeat = False
+
 
 # Function to check user input
 def play_again():
+    global repeat
     while True:
         replay = input("\nPress <enter> to play again, or <e> to exit"
-                       "\n>>> ").lower().strip()
+                       "\n>>> ")
 
         # If user presses <enter>, program sends out a looping signal
         if replay == "":
@@ -14,11 +17,12 @@ def play_again():
             print("Playing again...")
             break
         # If user presses <e>, program ends
-        elif replay == "e":
+        elif replay.lower().strip() == "e":
             repeat = False
             print("Exiting...")
             break
         # Else, question loops (I don't need to put an else statement in this case)
+
 
 # Main
 play_again()
